48eb3c47af5e33c32d6aeb901dfb43e4270e82a4
Lakshmi Batachari <lbatachari@hmc.edu>
2018-09-21 08:58:07 -0700

    init commit

---
+++ ELISA functions.R
@@ -0,0 +1,21 @@
+analyze <- function(oneFileName) {
+  
+  standards <- read_excel(oneFileName, sheet = 2)
+  OD450data <- read_excel(oneFileName, sheet = 3)
+  
+  meanStandards <- standards %>%
+    group_by(agConc)%>%
+    summarize(meanStandard = mean(OD450))
+  
+  linearFit <- lm(agConc~meanStandard, data = meanStandards)
+  
+  y_intercept <- linearFit$coefficients[1]
+  slope <- linearFit$coefficients[2]
+  
+  IL10Conc <- OD450data %>%
+    group_by(sample, dilutionFactor) %>%
+    summarize(meanValue = mean(OD450)) %>%
+    mutate(`concentration` = max(0, (meanValue*slope + y_intercept)*dilutionFactor))
+  
+  IL10Conc
+}
